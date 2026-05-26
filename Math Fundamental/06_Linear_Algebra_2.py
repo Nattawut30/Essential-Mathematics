31c0f8a5f82e704ca9217cca81edc9c0dccf99df
Nattawut30 <leonardo_90s@hotmail.com>
2026-05-26 19:55:17 +0700

    A little updated on matrix types

--- Math Fundamental/06_Linear_Algebra_2.py
+++ Math Fundamental/06_Linear_Algebra_2.py
@@ -35,3 +35,56 @@ print(determinant) # 1.0 block
 # orientation flip = i^ and j^ swap clockwise positions = the determinant will be negative!
 
 # 6.3: A negative Determinant
+from numpy.linalg import det
+from numpy import array
+
+i_hat = array([-2, 1])
+j_hat = array([1, 2])
+
+basis = array([i_hat, j_hat]).transpose()
+
+determinant = det(basis)
+
+print(determinant) # -5.0
+
+# if you have a determinant of 0, that means all of the space has been squished into a lesser dimension.
+# the area and volume respectively in both cases are 0!
+
+# 6.4: A determinant of zero
+from numpy.linalg import det
+from numpy import array
+
+i_hat = array([-2, 1])
+j_hat = array([3, -1.5])
+
+basis = array([i_hat, j_hat]).transpose()
+
+determinant = det(basis)
+
+print(determinant) # 0.0
+# So testing for a 0 determinant is highly helpful to determine if a transformation has linear dependence.
+
+""" 2. Special Types of Matrices """
+
+# Sqaure Matrix
+# a matrix that has an equal number of rows and columns
+# primarily used to represent linear transformations
+
+# Identity Matrix
+# a square matrix that has a diagonal of 1s while the other values are 0
+
+# Inverse Matrix
+# a matrix that undoes the transformation of another matrix
+# The inverse of matrix A is called A^-1
+
+# Diagonal Matrix
+# non-zero values while the rest of the values are 0
+
+# Triangular Matrix
+# A diagonal of non-zero values in front of a triangle of values while the rest of the values are 0
+
+# Sparse Matrix
+# The matrix that are mostly zero and have very few non-zero
+
+""" 3. Systems of Equations and Inverse Matrices """
+
