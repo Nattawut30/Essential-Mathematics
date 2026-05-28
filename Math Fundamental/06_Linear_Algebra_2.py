b28d690e361a0177de9afabd5a67738378ae709e
Nattawut30 <leonardo_90s@hotmail.com>
2026-05-28 20:06:11 +0700

    Finished CH6 go for CH 7

--- Math Fundamental/06_Linear_Algebra_2.py
+++ Math Fundamental/06_Linear_Algebra_2.py
@@ -88,3 +88,132 @@ print(determinant) # 0.0
 
 """ 3. Systems of Equations and Inverse Matrices """
 
+# Extract the coefiiciants into matrix A, the values on the right side of the equation into matrix B
+# AX = B
+ # 6.5: The inverse and identity matrix
+from sympy import *
+
+# 4x + 2y + 4z = 44
+# 5x + 3y + 7z = 56
+# 9x + 3y + 6z = 72
+
+A = Matrix([
+    [4, 2, 4],
+    [5, 3, 7],
+    [9, 3, 6],
+])
+
+# dot product between A and its inverse
+# will product identity function
+inverse = A.inv()
+identity = inverse * A
+
+print("INVERSE: {}".format(inverse))
+
+print("IDENTITY: {}".format(identity))
+
+# The lack of floating point precision will not affect our answers too badly,
+# 6.6: Numpy to solve a system of equations
+from numpy import array
+from numpy.linalg import inv
+
+# 4x + 2y + 4z = 44
+# 5x + 3y + 7z = 56
+# 9x + 3y + 6z = 72
+
+A = array([
+    [4, 2, 4],
+    [5, 3, 7],
+    [9, 3, 6],
+])
+
+B = array([
+    44,
+    56,
+    72
+])
+
+X = inv(A).dot(B)
+
+print(X) # [2. 34. -8]
+# So we knew it x = 2, y = 34, z = -8
+
+# 6.7: The full solution in SymPy to solve a system of equations
+from sympy import *
+
+# 4x + 2y + 4z = 44
+# 5x + 3y + 7z = 56
+# 9x + 3y + 6z = 72
+
+A = Matrix([
+    [4, 2, 4],
+    [5, 3, 7],
+    [9, 3, 6],
+])
+
+B = Matrix([
+    44,
+    56,
+    72
+])
+
+X = A.inv() * B
+
+print(X) # Matrix([[2], [34], [-8]])
+
+""" 4. Eigenvectors and Eigenvalues """
+
+# Matrix decompostion = breaking up a matrix into its basic components much like a factoring numbers
+# Useful for tasks like finding inverse matrices and calculating determinants
+
+# eigendecomposition is often used for ml and principle compenent analysis
+# helpful for breaking up a matrix into components that are easier to work with in different ml tasks
+# however, not all matrices can be decomposed into an eigenvector and eigenvalue
+
+# 6.8: Performing eigendecomposition in NumPy
+from numpy import array, diag
+from numpy.linalg import eig, inv
+
+A = array([
+    [1, 2],
+    [4, 5],
+])
+
+eigenvals, eigenvecs = eig(A)
+
+print("EIGENVALUES")
+print(eigenvals)
+print("\nEIGENVECTORS")
+print(eigenvecs)
+
+# reconstruct A
+# Diagonal form means the vector is padded into a matrix of zeroes and occupies the diagonal line in a similar
+# pattern to an identity matrix
+
+# 6.9: Decomposing and recomposing a matrix in NumPy
+from numpy import array, diag
+from numpy.linalg import eig, inv
+
+A = array([
+    [1, 2],
+    [4, 5]
+])
+
+eigenvals, eigenvecs = eig(A)
+
+print("EIGENVALUES")
+print(eigenvals)
+print("\nEIGENVECTORS")
+print(eigenvecs)
+
+print("\nREBUILD MATRIX")
+Q = eigenvecs
+R = inv(Q)
+
+L = diag(eigenvals)
+B = Q @ L @ R
+
+print(B)
+
+# As you can see the matrix we rebuild is the one we started with.
+
